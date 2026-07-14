59b8838b16cc9b120aa6a0332de19dd6c97c4a86
Chaitanyaagra <chaitanyaagrawal77@gmail.com>
2026-07-15 00:35:11 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='mhm-v20',R='mhm-r20';
+const C='mhm-v21',R='mhm-r21';
 const PRE=['./','./index.html','./manifest.json','./icon-192.png','./back-button-handler.js'];
 const CDN=['cdnjs.cloudflare.com','fonts.googleapis.com','fonts.gstatic.com','www.gstatic.com'];
 const SKIP=['firestore.googleapis.com','firebase.googleapis.com','identitytoolkit.googleapis.com','securetoken.googleapis.com'];
